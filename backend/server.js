e0abd0eed0aa8be324549a199e99b173e9680c66
MarshalTito7 <parijat.niyogy7@gmail.com>
2022-07-26 01:37:11 +0530

    get route documentation added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const {errorHandler} = require('./middleware/errorMiddleWare')
@@ -11,6 +13,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1', require('./routes/hallRoutes'))
 app.use('/api/v1', require('./routes/bookingRoutes'))
